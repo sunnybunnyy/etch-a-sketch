9f3bd601eb6567e40bc348cb418e3f00ba6edbed
sunnybunnyy <kradha6113@gmail.com>
2024-08-13 17:28:37 +0200

    Add button that allows user to change grid size

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function createGrid() {
-    const container = document.querySelector('#container');
-    for(let i = 0; i < 16; i++) {
+function createInitialGrid() {
+    const INITIAL_GRID_LENGTH = 4;
+    createGrid(4);
+    drawTrail();
+    const gridSize = document.querySelector('#grid-size');
+    gridSize.addEventListener('click', () => askGridSize());
+}
+
+function createGrid(gridLength) {
+    const container = document.querySelector('.container');
+    for(let i = 0; i < gridLength ** 2; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
+        square.style.width = (100 / gridLength).toString() + 'vw';
     }
-
 }
 
 function drawTrail() {
-    const squares = document.querySelectorAll('div.square');
+    const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         square.addEventListener('mouseenter', () => {
             square.style.backgroundColor = 'dimgray';
@@ -17,5 +25,26 @@ function drawTrail() {
     });
 }
 
-createGrid();
-drawTrail();
+function askGridSize() {
+    console.log("askGridSize() was called");
+
+    const gridLength = prompt(`Enter the number of squares per side for the grid.\nThe maximum is 100.`);
+    if (gridLength > 100) {
+        alert("Enter a number that is 100 or less.");
+        return;
+    }
+
+    console.log("gridLength = " + gridLength);
+    const body = document.querySelector('body');
+    const container = document.querySelector('.container');
+    body.removeChild(container);
+
+    const newContainer = document.createElement('div');
+    newContainer.classList.add('container');
+    body.appendChild(newContainer);
+
+    createGrid(gridLength);
+    drawTrail();
+}
+
+createInitialGrid();
